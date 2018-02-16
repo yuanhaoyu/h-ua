758714cf4fc7d03e769f5ade9188778e14e67b58
yuanhaoyu <yhy929768691@gmail.com>
2018-02-16 14:24:10 +0800

    add dist

---
+++ dist/h-ua.js
@@ -0,0 +1,70 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+require = (function (modules, cache, entry) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof require === "function" && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof require === "function" && require;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      function localRequire(x) {
+        return newRequire(localRequire.resolve(x));
+      }
+
+      localRequire.resolve = function (x) {
+        return modules[name][1][x] || x;
+      };
+
+      var module = cache[name] = new newRequire.Module;
+      modules[name][0].call(module.exports, localRequire, module, module.exports);
+    }
+
+    return cache[name].exports;
+  }
+
+  function Module() {
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({1:[function(require,module,exports) {
+"use strict";function n(n){for(let e=0;e<s.length;e++)if(s[e].n(n))return s[e].v;return"brower"}function e(n){return"micromessenger"==n.match(/MicroMessenger/i)}function r(n){return-1!==n.indexOf("mqqbrowser")}function t(n){return-1!==n.indexOf("android")}function o(n){return!!/iphone|ipad|ipod/.test(n)}function i(n){return-1!==n.indexOf("weibo")}Object.defineProperty(exports,"__esModule",{value:!0});const u=function(e){return n(navigator.userAgent.toLowerCase())},s=[{n:e,v:"wechat"},{n:r,v:"qq"},{n:i,v:"weibo"},{n:t,v:"android"},{n:o,v:"ios"}];window.UA=u,exports.default=u;
+},{}]},{},[1])
